05541dc5c208a2f4d25d30b18da2d015f7c97642
david <morenza@gmail.com>
2017-03-12 00:25:05 +0000

    Changed some transmission settings. Tweak local runs

--- app/organizer-base/image-x86/build.sh
+++ app/organizer-base/image-x86/build.sh
@@ -8,5 +8,5 @@ OPENSUBTITLES_PASSWORD=$1
 echo "$OPENSUBTITLES_USERNAME" >> $DIR/osdb.txt
 echo "$OPENSUBTITLES_PASSWORD" >> $DIR/osdb.txt
 
-docker build -t tvster/tvster-x86-base:v1.0.0 .
+docker build -t tvster/tvster-x86-base:v1.0.1 .
 rm $DIR/osdb.txt
